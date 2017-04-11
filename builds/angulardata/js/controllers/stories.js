bcfe62c0952e61b15005154f78b860b3ee3acc45
Akhil Nambiar <Akhil Nambiar>
2017-04-11 07:57:25 +0530

    point to proper screen

--- builds/angulardata/js/controllers/stories.js
+++ builds/angulardata/js/controllers/stories.js
@@ -1,10 +1,27 @@
-myApp.controller('StoriesController', ['$rootScope','$scope','$firebaseAuth','$firebaseArray', '$routeParams' ,
-	function($rootScope,$scope,$firebaseAuth,$firebaseArray,$routeParams) {
+myApp.controller('StoriesController', ['$rootScope','$scope',
+	'$firebaseAuth','$firebaseArray', '$routeParams' , '$firebaseObject',
+	function($rootScope,$scope,$firebaseAuth,$firebaseArray,$routeParams,$firebaseObject) {
 
  	$scope.storypoints = [];
 
 
  	$scope.whichStory = $routeParams.uId;
+
+ 	ref = firebase.database().ref()
+    .child('users').child('stories').child($scope.whichStory);
+
+    var storyNameRef = $firebaseObject(ref);
+    var storyName = '';
+
+    storyNameRef.$loaded().then(function(data) {
+        console.log(data.name);
+        $scope.storyName = data.name;
+        // DO WHATEVER
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
+
+    //console.log(storyName);
 
 	$scope.submitStory = function(data){
     console.log(data);
